07c31deef6f92344b73a432b815d5182ee404b62
JOSE VITOR OLIVEIRA REIS <11956726675@pedag.local>
2024-06-05 19:53:20 -0300

    brand new project

--- frios-project/src/components/HeaderBar.jsx
+++
@@ -1,9 +0,0 @@
-
-export default function HeaderBar() {
-    
-    return(
-        <div className="bg-primary w-full h-20 text-white px-12 py-2 flex items-center">
-            <h1>Frios & Calculistas</h1>
-        </div>
-    );
-}
