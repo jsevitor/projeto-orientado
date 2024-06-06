43b8e49b883251ea773a8c4392b1444e68d392ed
JOSE VITOR OLIVEIRA REIS <11956726675@pedag.local>
2024-06-05 22:01:09 -0300

    project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+// IMPORTAÇÃO DE COMPONENTS
+import Header from "./components/Header/Header";
+import MainContent from "./pages/MainContent/MainContent";
+
+// IMPORTAÇÃO DE ESTILOS
+import "./styles/Global.sass";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+
+      <MainContent />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,15 @@
+import "./Card.sass";
+
+const Card = ({ label, children }) => {
+  return (
+    <div className="card">
+      <div className="card_header">
+        <h2>{label}</h2>
+        <i className="bx bxs-truck"></i>
+      </div>
+      <form className="card_body">{children}</form>
+    </div>
+  );
+};
+
+export default Card;
